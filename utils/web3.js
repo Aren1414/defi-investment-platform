c32546be84e3b23e8c401ef2d579037f2dc616ec
Aren <71951258+Aren1414@users.noreply.github.com>
2025-05-23 17:28:32 +0330

    Update web3.js

--- utils/web3.js
+++ utils/web3.js
@@ -1,12 +1,10 @@
-import Web3 from "https://cdnjs.cloudflare.com/ajax/libs/web3/1.10.0/web3.min.js";
-
 let web3;
 
 if (typeof window !== "undefined" && window.ethereum) {
-  web3 = new Web3(window.ethereum);
+  web3 = new window.Web3(window.ethereum);
 } else {
-  const provider = new Web3.providers.HttpProvider("https://mainnet.base.org");
-  web3 = new Web3(provider);
+  const provider = new window.Web3.providers.HttpProvider("https://mainnet.base.org");
+  web3 = new window.Web3(provider);
 }
 
 export default web3;
