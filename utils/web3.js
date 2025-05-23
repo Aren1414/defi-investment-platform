7dbac3d160433cb2c13ca80eafcb46a723c474a0
Aren <71951258+Aren1414@users.noreply.github.com>
2025-05-23 17:42:28 +0330

    Update web3.js

--- utils/web3.js
+++ utils/web3.js
@@ -1,10 +1,9 @@
 let web3;
 
-if (typeof window !== "undefined" && window.ethereum) {
-  web3 = new window.Web3(window.ethereum);
+if (typeof window !== "undefined" && window.Web3) {
+  web3 = new window.Web3(window.ethereum || new window.Web3.providers.HttpProvider("https://mainnet.base.org"));
 } else {
-  const provider = new window.Web3.providers.HttpProvider("https://mainnet.base.org");
-  web3 = new window.Web3(provider);
+  console.error("Web3.js is not loaded. Make sure it is included in index.html.");
 }
 
 export default web3;
