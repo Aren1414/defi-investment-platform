bd4a43650f8b539e34c1a9803183bd44f086667b
Aren <71951258+Aren1414@users.noreply.github.com>
2025-05-23 17:06:13 +0330

    Update web3.js

--- utils/web3.js
+++ utils/web3.js
@@ -3,10 +3,12 @@ import Web3 from "web3";
 let web3;
 
 if (typeof window !== "undefined" && window.ethereum) {
+  // We are in the browser and MetaMask is running
   web3 = new Web3(window.ethereum);
 } else {
-  console.warn("MetaMask or Web3 provider is not available.");
-  web3 = null;
+  // We are on the server OR the user is not running MetaMask
+  const provider = new Web3.providers.HttpProvider("https://mainnet.base.org");
+  web3 = new Web3(provider);
 }
 
 export default web3;
