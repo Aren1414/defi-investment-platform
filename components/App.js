d819c5122ef4318b48cd1e08fe5130da696da61c
Aren <71951258+Aren1414@users.noreply.github.com>
2025-05-23 20:31:55 +0330

    Create App.js

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import InvestmentPage from "./Investment";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to DeFi Investment Platform 🚀</h1>
+      <InvestmentPage />
+    </div>
+  );
+}
